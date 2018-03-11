545ec370ac2905064e03afbe6072e28a3026ff57
1526853 <c.a.restituyonunez@wlv.ac.uk>
2018-03-10 23:17:28 -0400

    Added command prompt.

--- localPath.js
+++ localPath.js
@@ -8,19 +8,28 @@ let no_empty_name = false;
 const IMAGES_PATH = './images/';
 const OUTPUT_FILE_PATH = 'index.html';
 
-
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const workWithFile = () => {
     fs.readFile(FILE_PATH, (err, data) => {
         var oldData = data.toString();
         var newData = oldData.replace(PATTERN, IMAGES_PATH);
-        console.log(`Creating New File from ...${FILE_PATH}`);
+        console.log(`Creating New File from ${FILE_PATH}`);
         fs.open(OUTPUT_FILE_PATH, 'w+', (err, file) => {
             fs.writeFile(OUTPUT_FILE_PATH, newData, err => {
                 console.log(`New File Created at ${OUTPUT_FILE_PATH}`);
             });
         });
     });
-
 }
+
+rl.question('Insert name of the file you wish to transform:', (answer) => {
+    FILE_PATH = answer.toString();
+    workWithFile();
+});
+
+
 
